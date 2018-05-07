27294413f305ed54e10b17e8da3e91178e68e2e5
Anthony Lam <al3101@nyu.edu>
2018-05-07 01:47:05 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   result = nil
   min_value = Float::INFINITY 
-  name_hash.each do |item value|
+  name_hash.each do |item, value|
     if value > min_value 
       result = item 
       min_value = value 
