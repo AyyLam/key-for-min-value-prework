286b577464a754ea9b4b43a759a2cda01614b570
Anthony Lam <al3101@nyu.edu>
2018-05-07 01:45:01 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.each do |
+  result = nil
+  value = 
+  name_hash.each do |item value|
+    if 
+    
 
 end
