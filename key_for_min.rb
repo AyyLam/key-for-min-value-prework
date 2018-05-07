05642bcd0b307e3da0ed282308502a10d18bc379
Anthony Lam <al3101@nyu.edu>
2018-05-07 01:56:34 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   result = nil
   min_value = 0 
   name_hash.each do |item, value|
-    if value > min_value 
+    if value < min_value 
       result = item 
       min_value = value 
     end 
